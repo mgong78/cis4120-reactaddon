d3adc775f4b4ef9cbb29d7a6e6bc0ad357517832
mgong78 <68303067+mgong78@users.noreply.github.com>
2025-04-09 08:47:45 -0400

    created toggle functionality

--- weather.js
+++ weather.js
@@ -4,69 +4,74 @@ function ListItem({ taskName }) {
   return <p>{taskName}</p>;
 }
 
-function ToggleSection() {
-    const [isToggled, setIsToggled] = useState(false);
+function ToggleSection({actualTemps, feelTemps}) {
+    const { Switch, FormControlLabel } = MaterialUI;
+    const [isToggled, setIsToggled] = React.useState(false);
     const handleToggle = () => {
         setIsToggled(!isToggled); 
     };
-    return (
-        <Switch checked={isToggled} onChange={handleToggle} name="hi" />
-    );
-}
-          
-function App() {
-
+    let temps;
+    if (!isToggled) {
+        temps = actualTemps;
+    } else {
+        temps = feelTemps;
+    }
     return (
         <>
             <div id="main-weather-card">
                 <p>NOW</p>
                 <img className="icon-L" src="/assets/cloudy.png" />
-                <h3>41˚</h3>
+                <h3>{temps[0]}˚</h3>
             </div>
             <div id="side-section">
                 <div id="scroll-bar">
-                <div className="next-weather-card">
-                    <p>1PM</p>
-                    <img className="icon-M" src="/assets/partlycloudy.png" />
-                    <h4>43˚</h4>
-                </div>
-                <div className="next-weather-card">
-                    <p>2PM</p>
-                    <img className="icon-M" src="/assets/sunny.png" />
-                    <h4>45˚</h4>
-                </div>
-                <div className="next-weather-card">
-                    <p>3PM</p>
-                    <img className="icon-M" src="/assets/sunny.png" />
-                    <h4>44˚</h4>
-                </div>
-                <div className="next-weather-card">
-                    <p>4PM</p>
-                    <img className="icon-M" src="/assets/cloudy.png" />
-                    <h4>42˚</h4>
-                </div>
-                <div className="next-weather-card">
-                    <p>5PM</p>
-                    <img className="icon-M" src="/assets/cloudy.png" />
-                    <h4>38˚</h4>
-                </div>
-                <div className="next-weather-card">
-                    <p>6PM</p>
-                    <img className="icon-M" src="/assets/cloudy.png" />
-                    <h4>34˚</h4>
-                </div>
+                    <div className="next-weather-card">
+                        <p>1PM</p>
+                        <img className="icon-M" src="/assets/partlycloudy.png" />
+                        <h4>{temps[1]}˚</h4>
+                    </div>
+                    <div className="next-weather-card">
+                        <p>2PM</p>
+                        <img className="icon-M" src="/assets/sunny.png" />
+                        <h4>{temps[2]}˚</h4>
+                    </div>
+                    <div className="next-weather-card">
+                        <p>3PM</p>
+                        <img className="icon-M" src="/assets/sunny.png" />
+                        <h4>{temps[3]}˚</h4>
+                    </div>
+                    <div className="next-weather-card">
+                        <p>4PM</p>
+                        <img className="icon-M" src="/assets/cloudy.png" />
+                        <h4>{temps[4]}˚</h4>
+                    </div>
+                    <div className="next-weather-card">
+                        <p>5PM</p>
+                        <img className="icon-M" src="/assets/cloudy.png" />
+                        <h4>{temps[5]}˚</h4>
+                    </div>
+                    <div className="next-weather-card">
+                        <p>6PM</p>
+                        <img className="icon-M" src="/assets/cloudy.png" />
+                        <h4>{temps[6]}˚</h4>
+                    </div>
                 </div>
                 <div id="toggle">
-                <p>Actual Temperatures</p>
-                <label className="toggle">
-                    <input type="checkbox" id="btnToggle" name="btnToggle" />
-                    <span className="slider" />
-                </label>
-                <p>"Feels Like" Temperatures</p>
+                    <p>Actual Temperatures</p>
+                    <Switch checked={isToggled} onChange={handleToggle} name="hi" />
+                    <p>"Feels Like" Temperatures</p>
                 </div>
             </div>
         </>
+    );
+}
+          
+function App() {
+    const actualTemps = [41, 43, 45, 44, 42, 38, 34];
+    const feelTemps = [52, 54, 50, 46, 43, 36, 32];
 
+    return (
+        <ToggleSection actualTemps = {actualTemps} feelTemps={feelTemps}/>
     );
 }
  
